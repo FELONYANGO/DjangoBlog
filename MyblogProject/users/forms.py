51b6c53a8336aeedd2f097f6e8270ae4924e7667
FELONYANGO <felixonyango951@gmail.com>
2023-03-27 19:12:48 +0300

    registration

--- MyblogProject/users/forms.py
+++ MyblogProject/users/forms.py
@@ -1,6 +1,18 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=66)
     password = forms.CharField(max_length=150,widget=forms.PasswordInput)
+
+
+
+
+class RegisterForm(forms.Form):
+    class Meta:
+        model= User
+        fields=['username', 'password1','password2','email']
+        
+
 
--- MyblogProject/users/views.py
+++ MyblogProject/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.contrib.auth import login,authenticate,logout
-from .forms import  LoginForm
+from .forms import  LoginForm,RegisterForm
 
 #sign__in view
 def sign_in(request):
@@ -31,4 +31,9 @@ def sign_out(request):
     logout(request)
     messages.success(request, f'you have been signed out successfully')
     return redirect('login')
+
+def sign_up(request):
+    if request.method == 'GET':
+        form=RegisterForm()
+        return render(request, 'user/register.html', {'form': form})
     
